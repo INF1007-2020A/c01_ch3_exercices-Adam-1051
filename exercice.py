8bcbe02949b5bc3b2e7b70f6f4e891fb6c265492
Adam-1051 <adamadoui@gmail.com>
2020-09-14 18:35:32 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -29,7 +29,7 @@ def to_celsius(temperature: float) -> float:
 
 
 def to_farenheit(temperature: float) -> float:
-    Celsius=(temperature*(9/5)) + 32
+    Celsius=(temperature*(5/9)) -32
     return Celsius
 
 
